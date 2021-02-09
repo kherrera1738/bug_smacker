5db8a58c90f2dd7c7520bc6a4f60342ee611111e
Kevyn Herrera <kherrera1738@berkeley.edu>
2021-02-09 09:13:01 -0800

    added side bar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,31 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
-require("jquery")
-require("bootstrap")
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
+require("jquery");
+require("bootstrap");
+
+document.addEventListener("DOMContentLoaded", () => {
+  function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+
+  var openBtn = document.getElementById("openbtn");
+  openBtn.addEventListener("click", openNav, false);
+
+  var closeBtn = document.getElementById("closebtn");
+  closeBtn.addEventListener("click", closeNav, false);
+});
