8ea7b61b41943e0a1e370667fe4427d646fd64c0
Kevyn Herrera <kherrera1738@berkeley.edu>
2021-02-24 18:04:54 -0800

    fixed sign up issue

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,6 +9,10 @@ class RegistrationsController < Devise::RegistrationsController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
+    def sign_up_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
     def account_update_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
     end
